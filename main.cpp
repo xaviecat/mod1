fd8e26be90c24c6f39f5deb5adcc3ca067ca1b57
xcharra <xcharra@student.42lyon.fr>
2025-05-26 11:37:49 +0200

    Project configuration in QT

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.hpp"
+#include <iostream>
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    std::cout << "Hello cutie!" << std::endl;
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "mod1_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
