528ecf8a5ba89acdd89cec7b0f3a86932b5a22fd
xcharra <xcharra@student.42lyon.fr>
2025-05-26 17:59:52 +0200

    Refacto directory organisation, remove application translation (delete language file). Add resource files (maps). Simplify .gitignore

---
+++ srcs/main.cpp
@@ -0,0 +1,15 @@
+#include "gui/mainwindow.hpp"
+
+#include <iostream>
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+
+    std::cout << "Hello cutie!" << std::endl;
+
+    w.show();
+    return a.exec();
+}
