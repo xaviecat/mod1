74c23337c4eda09b64c667df951f355553edaef4
limastic <faust.nino@gmail.com>
2025-06-03 17:27:55 +0200

    added definition for Map class

---
+++ core/Map.cpp
@@ -0,0 +1,49 @@
+#include "Map.hpp"
+
+Map::Map(const std::string& filename) {
+    std::ifstream   infile(filename);
+    std::string     test;
+
+    if (!infile.is_open())
+        throw std::runtime_error("failed to open file: " + filename);
+
+    while (!infile.eof()){
+        infile >> test;
+        if (infile.eof()) break;
+        this->append(Map::_parseVertex(test));
+    }
+    infile.close();
+}
+
+QVector3D Map::_parseVertex(const std::string& vertex) {
+    std::stringstream stream(vertex);
+    float x,y,z;
+    char sep1,sep2;
+    char par1,par2;
+
+    if (vertex.find_first_of(')') != vertex.length() - 1
+        || vertex.find_last_of('(') != 0)
+        throw runtime_error(ERR_INPUT_FILE ERR_SYNTAX);
+    stream >> par1 >> x >> sep1 >> y >> sep2 >> z >> par2;
+    if (!stream || sep1 != ',' || sep2 != ',' || par1 != '(' || par2 != ')')
+        throw runtime_error(ERR_INPUT_FILE ERR_SYNTAX);
+
+    std::string rest;
+    stream >> rest;
+    if (stream)
+        throw runtime_error(ERR_INPUT_FILE ERR_SYNTAX);
+
+    return {x, y, z};
+}
+
+std::ostream &operator<<(ostream &o, const Map &rhs){
+    o << "{";
+
+    for (const auto &item: rhs){
+        if (item != rhs.first())
+            o << ", ";
+        o << "{" << item.x() << "," << item.y() << "," << item.z() << "}";
+    }
+    o << "}";
+    return o;
+}
---
+++ utils/errors.h
@@ -0,0 +1,7 @@
+#ifndef ERRORS_H
+# define ERRORS_H
+
+# define ERR_SYNTAX "Syntax error"
+# define ERR_INPUT_FILE "Unable to parse input file: "
+
+#endif
